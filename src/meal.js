5a9b4f17c406ec81596f7a0ab7903a049b2123c9
Sara Park <sspark106@gmail.com>
2023-02-01 14:31:26 -0700

    Remove all skips and pass meal tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,45 @@
-function nameMenuItem() {
-
+function nameMenuItem(food) {
+  return `Delicious ${food}`;
 }
 
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type
+  }
+}
+
+function addIngredients(ingredient, array) {
+  if (!array.includes(ingredient)) {
+    array.push(ingredient);
+  }
+}
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice.toString()}`;
+}
+
+function decreasePrice(originalPrice) {
+  var newPrice = originalPrice - (originalPrice * 0.10);
+  return newPrice; 
+}
+
+function createRecipe(title, array, type) {
+  return {
+    title: title,
+    ingredients: array,
+    type: type
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
