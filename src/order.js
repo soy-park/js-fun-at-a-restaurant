713a008b019fbfd51f335d315502fa9260d732dd
Sara Park <sspark106@gmail.com>
2023-02-02 12:15:10 -0700

    remove skips and pass order tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,37 @@
+function takeOrder(order, deliveryArray) {
+  if (deliveryArray.length < 3) {
+    deliveryArray.push(order);
+  }
+}
+
+function refundOrder(num, deliveryArray) {
+  for (var i = 0; i < deliveryArray.length; i++) {
+    if (deliveryArray[i].orderNumber === num) {
+      deliveryArray.splice(i, 1);
+    }
+  }
+}
+
+function listItems(deliveryArray) {
+  var items = [];
+  for (var i = 0; i < deliveryArray.length; i++) {
+    items.push(deliveryArray[i].item);
+  }
+  return items.join(", ");
+}
+
+function searchOrder(array, item) {
+  var foundItem = array.find(order => order.item === item);
+  if (foundItem) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
